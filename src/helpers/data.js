86f3354a7dcda50da206e997d9f488c916d1f8c1
Ian Harris <ianwebsterharris@gmail.com>
2018-06-11 21:23:36 -0600

    Data transformations on load

---
+++ src/helpers/data.js
@@ -0,0 +1,52 @@
+import _ from 'lodash';
+
+/**
+ * Parses and normalizes height (given in feet/inches), returning inches.
+ * @param {String} rawHeight
+ * @returns {Number} height in inches or undefined.
+ */
+function parseHeight(rawHeight = '') {
+  const matches = rawHeight.match(/^\D*(\d)\D*(\d+(?:\.5)?)?\D*$/);
+  return matches ? (Number(matches[1]) * 12) + Number(matches[2]) : undefined;
+}
+
+/**
+ * Parses and normalizes gender.
+ *
+ * @param {String} rawGender
+ * @returns {String} gender code, 'M' or 'F' or an empty string if it is missing.
+ */
+function parseGender(rawGender = '') {
+  const g = rawGender.charAt(0).toUpperCase();
+  return g === 'M' || g === 'F' ? g : '';
+}
+
+/**
+ * Parses a number from a string if possible.
+ * @param {*} value
+ */
+function parseNumber(value) {
+  const parsed = parseFloat(value);
+  return Number.isNaN(parsed) ? value : parsed;
+}
+
+/**
+ * Parse and normalize a row from the CSV.
+ *
+ * @param {Object} row raw row, parsed from CSV.
+ * @returns {Object} parsed CSV row.
+ */
+export default function parseRow(row) {
+  // Apply transformations to all row values
+  const mappedRow = _.mapValues(row, value =>
+    _.reduce([
+      _.trim,
+      parseNumber,
+    ], (acc, transform) => transform(acc), value));
+
+  // Apply column-specifc transformations
+  return Object.assign(mappedRow, {
+    height: parseHeight(row.height),
+    gender: parseGender(row.gender),
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,38 +1,20 @@
 import * as d3 from 'd3';
+import parseRow from './helpers/data';
 
 const csvPath = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTr8ODuAPw9TbuRntPn7cJSYnDMXitcwX-34guxTHdDj7xJuoSGmWxSgec4K0G9NulIFmiMMbOsSgpV/pub?gid=1355356545&single=true&output=csv';
 
-// d3.select('.chart')
-//   .selectAll('div')
-//   .data(data)
-//   .enter()
-//   .append('div')
-//   .style('width', d => `${d}px`)
-//   .text(d => d);
-
-
-// const sheetUrl = 'https://docs.google.com/spreadsheets/d/1qmL49UX6v1CvPnnx_Zt5niKZ9uilVvwh-apsmD8F3QI/edit?usp=sharing';
-
-// function draw({ data, tabletop }) {
-//   console.log(data, tabletop);
-// }
-
 function renderChart(data) {
-  console.log('rendering with data', data);
   d3.select('.chart')
     .selectAll('div')
     .data(data)
     .enter()
     .append('div')
-    .text(d => d.username);
+    .text(d => `${d.username} - ${d.gender} - ${d.height}`);
 }
 
 function fetchCsv() {
-  return d3.csv(csvPath)
-    // .then(data => console.log('data', data));
+  return d3.csv(csvPath, parseRow)
     .then(renderChart);
 }
 
-console.log('loading...');
-fetchCsv()
-  .then(a => console.log('done'));
+fetchCsv();
